d7fb8f8845ee487de6f877df3e59c132c80970ce
Rodrigo <Rodrigosbessa@gmail.com>
2026-03-15 22:52:29 +0000

    quarto commit

--- Projeto_final/settings.py
+++ Projeto_final/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -79,6 +80,8 @@ TEMPLATES = [
     },
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 WSGI_APPLICATION = 'Projeto_final.wsgi.application'
 
 
